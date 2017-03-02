93ee15c19785cad9bf0809b0f9f1cfdf604be57a
AJ Behncke <ajbinky@gmail.com>
2017-03-01 19:11:03 -0600

    Almost done

---
+++ AndrewBehnckeUnit7/AndrewBehnckeUnit7/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AndrewBehnckeUnit7
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(12, 226);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "&New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&XIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.Location = new System.Drawing.Point(12, 197);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(260, 26);
+            this.lblWinner.TabIndex = 2;
+            this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // formMain
+            // 
+            this.AcceptButton = this.btnNewGame;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "formMain";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        internal System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWinner;
+    }
+}
+
---
+++ AndrewBehnckeUnit7/AndrewBehnckeUnit7/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndrewBehnckeUnit7
+{
+    public partial class formMain : Form
+    {
+        Random randy = new Random();
+        Label[] lbl = new Label[9];
+        const int size = 3;
+        int[,] board = new int[size,size];
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lbl.Length; i++)
+            {
+                lbl[i] = new Label
+                {
+                    Location = new Point((25*i), (25*i)),
+                    Text = "Label " + i,
+                    AutoSize = true
+                    
+                };
+            }
+
+            foreach (Label l in lbl)
+            {
+                this.Controls.Add(l);
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j] = randy.Next(2);
+                }
+            }
+        }
+    }
+}
