8ec136c5ce40a807e3354af206b7ccb4ffc4471d
A.J. Behncke <A.J. Behncke>
2017-03-07 15:56:31 -0600

    finised unit 7

--- AndrewBehnckeUnit7/AndrewBehnckeUnit7/Form1.Designer.cs
+++ AndrewBehnckeUnit7/AndrewBehnckeUnit7/Form1.Designer.cs
@@ -31,11 +31,20 @@
             this.btnNewGame = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.lblWinner = new System.Windows.Forms.Label();
+            this.lblBoard1 = new System.Windows.Forms.Label();
+            this.lblBoard2 = new System.Windows.Forms.Label();
+            this.lblBoard3 = new System.Windows.Forms.Label();
+            this.lblBoard6 = new System.Windows.Forms.Label();
+            this.lblBoard5 = new System.Windows.Forms.Label();
+            this.lblBoard4 = new System.Windows.Forms.Label();
+            this.lblBoard9 = new System.Windows.Forms.Label();
+            this.lblBoard8 = new System.Windows.Forms.Label();
+            this.lblBoard7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnNewGame
             // 
-            this.btnNewGame.Location = new System.Drawing.Point(12, 226);
+            this.btnNewGame.Location = new System.Drawing.Point(12, 188);
             this.btnNewGame.Name = "btnNewGame";
             this.btnNewGame.Size = new System.Drawing.Size(75, 23);
             this.btnNewGame.TabIndex = 0;
@@ -45,7 +54,8 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(197, 226);
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(99, 188);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 1;
@@ -55,11 +65,101 @@
             // 
             // lblWinner
             // 
-            this.lblWinner.Location = new System.Drawing.Point(12, 197);
+            this.lblWinner.Location = new System.Drawing.Point(12, 159);
             this.lblWinner.Name = "lblWinner";
-            this.lblWinner.Size = new System.Drawing.Size(260, 26);
+            this.lblWinner.Size = new System.Drawing.Size(159, 26);
             this.lblWinner.TabIndex = 2;
             this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard1
+            // 
+            this.lblBoard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard1.Location = new System.Drawing.Point(12, 9);
+            this.lblBoard1.Name = "lblBoard1";
+            this.lblBoard1.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard1.TabIndex = 3;
+            this.lblBoard1.Text = "X";
+            this.lblBoard1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard2
+            // 
+            this.lblBoard2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard2.Location = new System.Drawing.Point(68, 9);
+            this.lblBoard2.Name = "lblBoard2";
+            this.lblBoard2.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard2.TabIndex = 4;
+            this.lblBoard2.Text = "X";
+            this.lblBoard2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard3
+            // 
+            this.lblBoard3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard3.Location = new System.Drawing.Point(124, 9);
+            this.lblBoard3.Name = "lblBoard3";
+            this.lblBoard3.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard3.TabIndex = 5;
+            this.lblBoard3.Text = "X";
+            this.lblBoard3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard6
+            // 
+            this.lblBoard6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard6.Location = new System.Drawing.Point(124, 59);
+            this.lblBoard6.Name = "lblBoard6";
+            this.lblBoard6.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard6.TabIndex = 8;
+            this.lblBoard6.Text = "X";
+            this.lblBoard6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard5
+            // 
+            this.lblBoard5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard5.Location = new System.Drawing.Point(68, 59);
+            this.lblBoard5.Name = "lblBoard5";
+            this.lblBoard5.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard5.TabIndex = 7;
+            this.lblBoard5.Text = "X";
+            this.lblBoard5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard4
+            // 
+            this.lblBoard4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard4.Location = new System.Drawing.Point(12, 59);
+            this.lblBoard4.Name = "lblBoard4";
+            this.lblBoard4.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard4.TabIndex = 6;
+            this.lblBoard4.Text = "X";
+            this.lblBoard4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard9
+            // 
+            this.lblBoard9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard9.Location = new System.Drawing.Point(124, 109);
+            this.lblBoard9.Name = "lblBoard9";
+            this.lblBoard9.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard9.TabIndex = 11;
+            this.lblBoard9.Text = "X";
+            this.lblBoard9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard8
+            // 
+            this.lblBoard8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard8.Location = new System.Drawing.Point(68, 109);
+            this.lblBoard8.Name = "lblBoard8";
+            this.lblBoard8.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard8.TabIndex = 10;
+            this.lblBoard8.Text = "X";
+            this.lblBoard8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBoard7
+            // 
+            this.lblBoard7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoard7.Location = new System.Drawing.Point(12, 109);
+            this.lblBoard7.Name = "lblBoard7";
+            this.lblBoard7.Size = new System.Drawing.Size(50, 50);
+            this.lblBoard7.TabIndex = 9;
+            this.lblBoard7.Text = "X";
+            this.lblBoard7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // formMain
             // 
@@ -67,7 +167,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(183, 219);
+            this.Controls.Add(this.lblBoard9);
+            this.Controls.Add(this.lblBoard8);
+            this.Controls.Add(this.lblBoard7);
+            this.Controls.Add(this.lblBoard6);
+            this.Controls.Add(this.lblBoard5);
+            this.Controls.Add(this.lblBoard4);
+            this.Controls.Add(this.lblBoard3);
+            this.Controls.Add(this.lblBoard2);
+            this.Controls.Add(this.lblBoard1);
             this.Controls.Add(this.lblWinner);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnNewGame);
@@ -83,6 +192,15 @@
         private System.Windows.Forms.Button btnNewGame;
         internal System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Label lblBoard1;
+        private System.Windows.Forms.Label lblBoard2;
+        private System.Windows.Forms.Label lblBoard3;
+        private System.Windows.Forms.Label lblBoard6;
+        private System.Windows.Forms.Label lblBoard5;
+        private System.Windows.Forms.Label lblBoard4;
+        private System.Windows.Forms.Label lblBoard9;
+        private System.Windows.Forms.Label lblBoard8;
+        private System.Windows.Forms.Label lblBoard7;
     }
 }
 
--- AndrewBehnckeUnit7/AndrewBehnckeUnit7/Form1.cs
+++ AndrewBehnckeUnit7/AndrewBehnckeUnit7/Form1.cs
@@ -13,33 +13,13 @@ namespace AndrewBehnckeUnit7
     public partial class formMain : Form
     {
         Random randy = new Random();
-        Label[] lbl = new Label[9];
         const int size = 3;
         int[,] board = new int[size,size];
+        Label[,] labels = new Label[size,size];
 
         public formMain()
         {
             InitializeComponent();
-        }
-
-        private void formMain_Load(object sender, EventArgs e)
-        {
-            for (int i = 0; i < lbl.Length; i++)
-            {
-                lbl[i] = new Label
-                {
-                    Location = new Point((25*i), (25*i)),
-                    Text = "Label " + i,
-                    AutoSize = true
-                    
-                };
-            }
-
-            foreach (Label l in lbl)
-            {
-                this.Controls.Add(l);
-            }
-
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -53,9 +33,72 @@ namespace AndrewBehnckeUnit7
             {
                 for (int j = 0; j < size; j++)
                 {
-                    board[i, j] = randy.Next(2);
+                    int randomInt = randy.Next(2);
+                    board[i, j] = randomInt;
+                    changeLabel(labels[i, j], randomInt);
                 }
             }
+
+            int x = countRows(1);
+            int o = countRows(0);
+            if (x - o == 0) lblWinner.Text = "Tie";
+            if (x - o < 0) lblWinner.Text = "O Wins";
+            if (x - o > 0) lblWinner.Text = "X Wins";
+
+        }
+
+        /**
+         *  Changes the label text to either "O" or "X" depending on given int i
+         **/
+        private void changeLabel(Label l, int i)
+        {
+            if (i == 0) l.Text = "O";
+            if (i == 1) l.Text = "X";
+            return;
+        }
+
+
+        private int countRows(int i)
+        {
+            int count = 0;
+
+            // Rows
+            for (int x = 0; x < size; x++)
+            {
+                if (board[x,0] == i && board[x,1] == i && board[x,2] == i)
+                {
+                    count++;
+                }
+            }
+
+            // Columns
+            for (int x = 0; x < size; x++)
+            {
+                if (board[0, x] == i && board[1, x] == i && board[2, x] == i)
+                {
+                    count++;
+                }
+            }
+
+            // Diagonals
+            if (((board[0,0] == i && board[2,2] == i) || (board[0,2] == i && board[2,0] == i)) && board[1,1] == i)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            labels[0, 0] = lblBoard1;
+            labels[0, 1] = lblBoard2;
+            labels[0, 2] = lblBoard3;
+            labels[1, 0] = lblBoard4;
+            labels[1, 1] = lblBoard5;
+            labels[1, 2] = lblBoard6;
+            labels[2, 0] = lblBoard7;
+            labels[2, 1] = lblBoard8;
+            labels[2, 2] = lblBoard9;
         }
     }
 }
